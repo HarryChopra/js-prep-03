1641dad2bc4dc256254d85ce9511bb6fe5c09a35
Harry Chopra <harrychopra@gmail.com>
2020-02-08 19:19:18 +0000

    done

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-var animal = 'dog'
+var animal = 'dog';
 
 function myAnimal() {
-  // You should not need to modify this function
-  return animal
+	// You should not need to modify this function
+	return animal;
 }
 
 function yourAnimal() {
-  // The tests expect this function to return `animal` just like the previous function
-  // However, you cannot simply modify the existing variable declared on line 1 in the global scope
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: Hard-coding 'cat' below will not work
-  let animal = 'cat'
-  return animal
+	// The tests expect this function to return `animal` just like the previous function
+	// However, you cannot simply modify the existing variable declared on line 1 in the global scope
+	// How can we make sure that this function
+	// and the above function both pass?
+	// P.S.: Hard-coding 'cat' below will not work
+	let animal = 'cat';
+	return animal;
 }
 
 function add2(n) {
-   // Feel free to move things around!
-  const two = 2
-   return n + two
-
+	// Feel free to move things around!
+	const two = 2;
+	return n + two;
 }
 
 var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
-}
+	return function() {
+		return 'FUNKY!';
+	};
+};
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()()
+var theFunk = funkyFunction()();
 
-
-
-module.exports = {animal, myAnimal, yourAnimal, add2, funkyFunction, theFunk}
+export { animal, myAnimal, yourAnimal, funkyFunction, theFunk, add2 };
--- test/index.test.js
+++ test/index.test.js
@@ -1,48 +1,48 @@
-const index = require("../index.js")
+import * as index from '../index.js';
 
+// console.log(myAnimal());
 describe('Fix the Scope', function() {
-  describe('myAnimal()', function() {
-    test('returns the animal variable', () => {
-      expect(index.myAnimal()).toBe('dog')
-    })
+	describe('myAnimal()', function() {
+		test('returns the animal variable', () => {
+			expect(index.animal).toBe('dog');
+		});
 
-    test('does not modify the  animal variable', () => {
-      expect(index.myAnimal.toString()).not.toContain("animal =")
-    })
-  })
+		test('does not modify the  animal variable', () => {
+			expect(index.myAnimal().toString()).not.toContain('animal =');
+		});
+	});
 
-  describe('yourAnimal()', function() {
-    test('returns your animal', function(){
-      expect(index.yourAnimal()).toBe('cat')
-    })
+	describe('yourAnimal()', function() {
+		test('returns your animal', function() {
+			expect(index.yourAnimal()).toBe('cat');
+		});
 
-    test('does not hard-code the answer', function() {
-      expect(index.yourAnimal.toString()).toContain("return animal")
-      expect(index.yourAnimal.toString()).not.toContain("return 'cat'")
-    })
+		test('does not hard-code the answer', function() {
+			expect(index.yourAnimal.toString()).toContain('return animal');
+			expect(index.yourAnimal.toString()).not.toContain("return 'cat'");
+		});
 
-    test('does not change the output of the myAnimal function', () => {
-      expect(index.myAnimal()).toBe('dog')
-    })
-  })
+		test('does not change the output of the myAnimal function', () => {
+			expect(index.myAnimal()).toBe('dog');
+		});
+	});
 
-  describe('add2(n)', function() {
-    test('adds two to n', function() {
-      const n = Math.floor(Math.random() * 1000)
-      expect(index.add2(n)).toBe(n + 2)
-    })
-  })
+	describe('add2(n)', function() {
+		test('adds two to n', function() {
+			const n = Math.floor(Math.random() * 1000);
+			expect(index.add2(n)).toBe(n + 2);
+		});
+	});
 
-  describe('funkyFunction()', function() {
-    test('returns a function', function() {
-      expect(typeof index.funkyFunction()).toBe('function')
-    })
-  })
+	describe('funkyFunction()', function() {
+		test('returns a function', function() {
+			expect(typeof index.funkyFunction()).toBe('function');
+		});
+	});
 
-  describe("the variable 'theFunk'", function() {
-    test('is equal to "FUNKY!"', function() {
-      expect(index.theFunk).toBe('FUNKY!')
-    })
-  })
-
-})
+	describe("the variable 'theFunk'", function() {
+		test('is equal to "FUNKY!"', function() {
+			expect(index.theFunk).toBe('FUNKY!');
+		});
+	});
+});
